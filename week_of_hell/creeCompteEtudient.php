b846e7d09b801d184dd82aa4eeb9bfe316db01dd
Rajdi <rajdimucanji16@gmail.com>
2025-03-17 11:11:24 +0100

    0

--- week_of_hell/creeCompteEtudient.php
+++ week_of_hell/creeCompteEtudient.php
@@ -12,14 +12,21 @@
     <form action="creeCompteEtudient.php" method="post">
         <label for="nom">Nom</label>
         <input type="text" name="nom" id="nom" required>
+
         <label for="prenom">Prenom</label>
         <input type="text" name="prenom" id="prenom" required>
+
         <label for="email">Email</label>
         <input type="email" name="email" id="email" required>
+
         <label for="password">Mot de passe</label>
         <input type="password" name="password" id="password" required>
+
         <label for="password2">Confirmer mot de passe</label>
         <input type="password" name="password2" id="password2" required>
+        <label for="Votre CV"></label>
+        <input type="file" name="file" id="file">
+
         <input type="submit" value="Crée compte">
     </form>
 
