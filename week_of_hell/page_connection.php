a99816ac0d9f75ba82dd2facc0610076f963c154
MartinLe0 <leomartin060905pgmail.com>
2025-03-17 10:34:59 +0100

    connection v1

--- week_of_hell/page_connection.php
+++ week_of_hell/page_connection.php
@@ -1,8 +1,41 @@
 <?php
     include('./config.php');
     if(isset($_POST['Connexion'])){
-        if(!empty($_POST['identifiant']) && !empty($_POST['motdepasse'])){
-            
+        if(!empty($_POST['email']) && !empty($_POST['motdepasse']) && !empty($_POST['typeCompte'])){
+            switch($_POST['typeCompte']){
+                case "etudiant":
+                    $stmt = $conn->query("SELECT * FROM compteetudiant");
+                    $stmt->execute();
+                    $etudiants = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    foreach($etudiants as $etudiant){
+                        if($etudiant['email']===$_POST['email'] && $etudiant['motDePasse']===$_POST['motdepasse']){
+                            header("location : page.php");
+                        }
+                    }
+                    break;
+
+                case "entreprise":
+                    $stmt = $conn->query("SELECT * FROM comptentreprise");
+                    $stmt->execute();
+                    $entreprises = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    foreach($entreprises as $entreprise){
+                        if($entreprise['email']===$_POST['email'] && $entreprise['motDePasse']===$_POST['motdepasse']){
+                            header("location : page.php");
+                        }
+                    }
+                    break;
+
+                case "professeur":
+                    $stmt = $conn->query("SELECT * FROM compteprofesseur");
+                    $stmt->execute();
+                    $professeurs = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    foreach($professeurs as $professeur){
+                        if($professeur['email']===$_POST['email'] && $professeurs['motDePasse']===$_POST['motdepasse']){
+                            header("location : page.php");
+                        }
+                    }
+                    break;
+            }
         }else{
             echo "veulliez remplir tout les champs";
         }
@@ -22,7 +55,7 @@
     <div>
         <h1>Connection</h1>
         <form method="post">
-            <input type="text" name="identifiant" placeholder="Email" required>
+            <input type="text" name="email" placeholder="Email" required>
             <input type="password" name="motdepasse" placeholder="Mot de passe" required>
             <label for="typeCompte">Etudiant</label>
             <input type="radio" name="typeCompte" value="etudiant">
