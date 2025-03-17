510488df537637550c0028bdea76b4113f488ca5
MartinLe0 <leomartin060905pgmail.com>
2025-03-17 11:43:53 +0100

    connectino ok

--- week_of_hell/page_connection.php
+++ week_of_hell/page_connection.php
@@ -9,7 +9,7 @@
                     $etudiants = $stmt->fetchAll(PDO::FETCH_ASSOC);
                     foreach($etudiants as $etudiant){
                         if($etudiant['email']===$_POST['email'] && $etudiant['motDePasse']===$_POST['motdepasse']){
-                            header("location : page.php");
+                            header("location: page.php");
                         }
                     }
                     break;
@@ -20,7 +20,8 @@
                     $entreprises = $stmt->fetchAll(PDO::FETCH_ASSOC);
                     foreach($entreprises as $entreprise){
                         if($entreprise['email']===$_POST['email'] && $entreprise['motDePasse']===$_POST['motdepasse']){
-                            header("location : creeCompteEntreprise.php");
+                            header("location: page.php");
+
                         }
                     }
                     break;
@@ -31,7 +32,7 @@
                     $professeurs = $stmt->fetchAll(PDO::FETCH_ASSOC);
                     foreach($professeurs as $professeur){
                         if($professeur['email']===$_POST['email'] && $professeurs['motDePasse']===$_POST['motdepasse']){
-                            header("location : page.php");
+                            header("location: page.php");
                         }
                     }
                     break;
