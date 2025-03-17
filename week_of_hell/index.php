af2d634adc848e849294c2f3deea1d04b7e2416c
Rajdi <rajdimucanji16@gmail.com>
2025-03-17 15:09:16 +0100

    0

--- week_of_hell/index.php
+++ week_of_hell/index.php
@@ -4,13 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <div class="barre">
-        <a href="index.php">Acceuil</a>
-        <a href="page_connection.php">Conecter vous</a>    
-         
+        <a href="index.php">Accueil</a>
+        <a href="page_connection.php">Connectez-vous</a>
+        <a href="page_compte.php">Compte</a>
+        <a href="page_stages.php">Stages</a>
+        <form action="recherche.php" method="get" class="search-form">
+            <input type="text" name="query" placeholder="Rechercher un stage...">
+            <button type="submit">Rechercher</button>
+        </form>
     </div>
 </body>
 <footer>
