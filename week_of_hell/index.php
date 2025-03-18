9d491ef25eee2e126043be573d176706929fcebe
Bernel <ryan.bernel15@gmail.com>
2025-03-18 14:47:54 +0100

    NONNNNNNNNNNNNNNN!!!!!!!!!!!!!!!!

--- week_of_hell/index.php
+++ week_of_hell/index.php
@@ -13,21 +13,25 @@
 <body>
     <div class="barre">
         <a href="index.php">Accueil</a>
+        <?php if (isset($_SESSION["status"])): ?>
+                    <a href="deconnexion.php">Deconnexion</a>
+                <?php else: ?>
+                    <a href="page_connection.php">Connectez-vous</a>
+                <?php endif ?> 
             <?php if (isset($_SESSION["status"])): ?>
                     <?php if($_SESSION["status"]==="entreprise"||$_SESSION["status"]==="etudiant"|| $_SESSION["status"]==="professeur"):?>
                         <a href="page_compte.php">Compte</a>
                     <?php endif ?>
                 <?php endif ?>
                 <?php if (isset($_SESSION["status"])): ?>
-                    <?php if($_SESSION["status"]==="entreprise"):?>
+                    <?php if($_SESSION["status"]==="entreprise"||$_SESSION["status"]==="etudiant"):?>
                         <a href="creeOffreStage.php">Offre Stages</a>
                     <?php endif ?>
-                    <?php if ($_SESSION["status"]==="etudiant"):?> 
+                    <?php if ($_SESSION["status"]==="etudiant"||$_SESSION["status"]==="etudiant" ):?> 
                         <a href="creeDemandeStage.php">Demande Stages</a>
                     <?php endif ?>
                 
-            <div class = "OffredeStage">    
-                    
+            <div class = "OffredeStage">                     
                         <?php if($_SESSION["status"]==="etudiant"):?>
                             <h2>Offre de stage</h2> 
                         <?php endif ?>
@@ -39,12 +43,7 @@
                             <h2>Demande de stage</h2>
                     <?php endif ?>
                 <?php endif ?>
-            </div>        
-                <?php if (isset($_SESSION["status"])): ?>
-                    <a href="deconnexion.php">Deconnexion</a>
-                <?php else: ?>
-                    <a href="page_connection.php">Connectez-vous</a>
-                <?php endif ?>      
+            </div>              
     </div>
             
         
