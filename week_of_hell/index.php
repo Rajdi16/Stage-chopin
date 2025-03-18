76f64597116748d5199f54b87fad6803ca4c13a9
Bernel <ryan.bernel15@gmail.com>
2025-03-18 11:06:21 +0100

    Suppression de certaine données dependant du statu

--- week_of_hell/index.php
+++ week_of_hell/index.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start("status");
 ?>
 <html lang="en">
 
@@ -15,10 +15,19 @@
         <a href="index.php">Accueil</a>
         <a href="page_connection.php">Connectez-vous</a>
         <a href="page_compte.php">Compte</a>
+        <?php if($_SESSION["status"]="etudiant"):?>
         <a href="creeOffreStage.php">Offre Stages</a>
+        <?php endif ?>
+        <?php if ($_SESSION["status"]="entrprise"):?> 
         <a href="creeDemandeStage.php">Demande Stages</a>
+        <?php endif ?>
 
-
+    </div>
+    <div class = "OffredeStage">
+        <h2>Offre de stage</h2>
+    </div>
+    <div class = "DemandedeStage">
+        <h2>Demande de stage</h2>
     </div>
 </body>
 <footer>
