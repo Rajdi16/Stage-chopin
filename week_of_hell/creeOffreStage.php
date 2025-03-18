69e46771128897d3331467c72deb0e7d12fde6c9
Rajdi <rajdimucanji16@gmail.com>
2025-03-18 10:02:07 +0100

    0

--- week_of_hell/creeOffreStage.php
+++ week_of_hell/creeOffreStage.php
@@ -1,15 +1,20 @@
 <?php
 include('./config.php');
 if (isset($_POST['cree'])) {
-    if (!empty($_POST['dateDeb']) && !empty($_POST['dateFin']) && !empty($_POST['description']) && !empty($_POST['etat']) && !empty($_POST['entreprise_Id'])) {
-        $stmt = $conn->prepare("INSERT INTO offrestage (`dateDeb`, `dateFin`, `description`, `etat`, `entreprise_Id`) VALUES (:dateDeb, :dateFin, :description, :etat, :entreprise_Id)");
+    if (!empty($_POST['dateDeb']) && !empty($_POST['dateFin']) && !empty($_POST['detail']) && !empty($_POST['etat']) && !empty($_POST['entreprise_Id'])) {
+        $stmt = $conn->prepare("INSERT INTO offrestage (`dateDeb`, `dateFin`, `detail`, `etat`, `entreprise_Id`) VALUES (:dateDeb, :dateFin, :detail, :etat, :entreprise_Id)");
         $stmt->bindParam(':dateDeb', $_POST['dateDeb']);
         $stmt->bindParam(':dateFin', $_POST['dateFin']);
-        $stmt->bindParam(':description', $_POST['description']);
-        $stmt->bindParam(':etat', $_POST['etat']);
-        $stmt->bindParam(':entreprise_Id', $_POST['entreprise_Id']);
-        $stmt->execute();
-        echo "element ajouter";
+        if ($_POST['dateDeb'] > $_POST['dateFin']) {
+            echo "la date de debut doit etre inferieur a la date de fin";
+            return;
+        } else {
+            $stmt->bindParam(':detail', $_POST['detail']);
+            $stmt->bindParam(':etat', $_POST['etat']);
+            $stmt->bindParam(':entreprise_Id', $_POST['entreprise_Id']);
+            $stmt->execute();
+        }
+        header("Location: index.php");
     } else {
         echo "remplissez tout les champs";
     }
@@ -28,21 +33,22 @@ if (isset($_POST['cree'])) {
 <body class="container">
     <div>
         <h1>Offere un stage</h1>
-        <form action="creeUnStage.php" method="post">
+        <form action="creeOffreStage.php" method="post">
+
             <label for="dateDeb">Date de debut</label>
             <input type="date" name="dateDeb" id="dateDeb" required>
 
             <label for="dateFin">Date de fin</label>
             <input type="date" name="dateFin" id="dateFin" required>
 
-            <label for="description">Description</label>
-            <input type="text" name="description" id="description" required>
+            <label for="detail">Description</label>
+            <input type="text" name="detail" id="detail" required>
 
             <label for="etat">Etat</label>
             <input type="text" name="etat" id="etat" required>
 
             <label for="entreprise_Id">Entreprise Id</label>
-            <input type="text" name="entreprise_Id" id="entreprise_Id" required>
+            <input type="number" name="entreprise_Id" id="entreprise_Id" required>
 
 
             <input type="submit" name="cree">
