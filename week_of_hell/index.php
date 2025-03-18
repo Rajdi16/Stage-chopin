aeb70ff014102f609d7c6dbb85c819e75af18581
Bernel <ryan.bernel15@gmail.com>
2025-03-18 15:14:36 +0100

    0

--- week_of_hell/index.php
+++ week_of_hell/index.php
@@ -14,27 +14,27 @@
     <div class="barre">
         <a href="index.php">Accueil</a>
         <?php if (isset($_SESSION["status"])): ?>
-            <?php if (isset($_SESSION["status"])): ?>
-                    <?php if($_SESSION["status"]==="entreprise"||$_SESSION["status"]==="etudiant"|| $_SESSION["status"]==="professeur"):?>
-                        <a href="page_compte.php">Compte</a>
-                    <?php endif ?>
-                <?php endif ?>
-                    <a href="deconnexion.php">Deconnexion</a>
-                <?php else: ?>
-                    <a href="page_connection.php">Connectez-vous</a>
-                <?php endif ?> 
-                <?php if (isset($_SESSION["status"])): ?>
-                    <?php if($_SESSION["status"]==="entreprise"||$_SESSION["status"]==="etudiant"):?>
-                        <a href="creeOffreStage.php">Offre Stages</a>
-                    <?php endif ?>
-                    <?php if ($_SESSION["status"]==="etudiant"||$_SESSION["status"]==="etudiant" ):?> 
-                        <a href="creeDemandeStage.php">Demande Stages</a>
-                    <?php endif ?>
+            <?php if($_SESSION["status"]==="entreprise"||$_SESSION["status"]==="etudiant"|| $_SESSION["status"]==="professeur"):?>
+                <a href="page_compte.php">Compte</a>
+            <?php endif ?>
+        <?php endif ?>
+        <?php if (isset($_SESSION["status"])): ?>
+            <a href="deconnexion.php">Deconnexion</a>
+        <?php else: ?>
+            <a href="page_connection.php">Connectez-vous</a>
+        <?php endif ?> 
+        <?php if (isset($_SESSION["status"])): ?>
+            <?php if($_SESSION["status"]==="entreprise"||$_SESSION["status"]==="etudiant"):?>
+                <a href="creeOffreStage.php">Offre Stages</a>
+            <?php endif ?>
+            <?php if ($_SESSION["status"]==="etudiant"||$_SESSION["status"]==="etudiant" ):?> 
+                <a href="creeDemandeStage.php">Demande Stages</a>
+            <?php endif ?>
+        <?php endif ?>
                 
             <div class = "OffredeStage">                     
-                        <?php if($_SESSION["status"]==="etudiant"):?>
-                            <h2>Offre de stage</h2> 
-                        <?php endif ?>
+            <?php if($_SESSION["status"]==="etudiant"):?>
+                <h2>Offre de stage</h2> 
             <?php endif ?>
             </div> 
             <div class = "DemandedeStage">
